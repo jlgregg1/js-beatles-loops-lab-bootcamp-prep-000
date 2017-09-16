fd737eea2642aa91b8ce0f6c966b9469fb781646
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-16 16:44:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    if (i===0); {array.push("John Lennon plays guitar")}
+    if (i===0) {array.push("John Lennon plays guitar")};
     else {array.push("musicians[${i}] plays instruments[${i}]"};
   }
   return array;
