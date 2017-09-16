dfa9a95e17a3f93fec3e8926048172c856daff65
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-16 16:28:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (var i = 0, i < array.length, i++) {
+  for (var i = 0, i < musicians.length, i++) {
     if (i===0) {array.push("John Lennon plays guitar")}
     else {array.push("musicians[${i}]) plays instruments[${i}]"}
   }
