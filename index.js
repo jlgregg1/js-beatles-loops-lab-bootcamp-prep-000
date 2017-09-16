5c20529ea41373c377c1015f314dd36d76593632
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-16 17:08:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (let i = 0; i < musicians.length; i++) {
@@ -9,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
     // else {
     //   array.push(`musicians[${i}] plays instruments[${i}])`
     //   }
-array.push(`musicians[${i}] plays instruments[${i}]`)
+array.push(`"musicians[${i}]" plays "instruments[${i}]"`)
   }
   return array
 }
