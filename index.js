e5489da9d859a3ae8b2d5477a9e7a21b0ebee9ee
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-16 17:12:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
     // else {
     //   array.push(`musicians[${i}] plays instruments[${i}])`
     //   }
-array.push(`musicians[parseInt(${i})] plays instruments[parseInt(${i})]`)
+array.push(`musicians[${i}] plays instruments[${i}]`)
   }
   return array
 }
